e5db0147ed62034f025f55a5d2c9e6f4a619c2e6
Paras <parasparte653@gmail.com>
2026-06-04 10:43:50 +0530

    kadanes

---
+++ Array/subarray/kadanes.java
@@ -0,0 +1,29 @@
+
+public class kadanes{
+
+public static void kedanes(int num[]){
+
+int max=Integer.MIN_VALUE;
+int cs=0;
+
+for(int i=0;i<num.length;i++){
+cs=cs+num[i];
+max=Math.max(max,cs);
+if(cs<0){
+cs=0;
+}
+
+	
+}
+System.out.println("our max subarray sum is : " +max);
+}
+
+public static void main(String args[]){
+
+int num []={-2,-3,4,-1,-2,1,5,-3};
+kedanes(num);
+
+
+}
+
+}
