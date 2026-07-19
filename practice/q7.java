5761973a9da724766274607e73bdd518d67342bf
Paras <parasparte653@gmail.com>
2026-07-19 11:27:07 +0530

    q7

---
+++ practice/q7.java
@@ -0,0 +1,28 @@
+// Reverse an array in place
+
+import java.util.*;
+
+public class q7{
+
+
+public static void reverse(int arr[]){
+int lp=0;
+int rp=arr.length-1;
+while(lp<rp){
+int temp=arr[lp];
+arr[lp]=arr[rp];
+arr[rp]=temp;
+lp++;
+rp--;
+}
+System.out.print("reverse arr=" + Arrays.toString(arr));
+
+}
+
+public static void main(String args[]){
+
+int arr[]={1,2,3,4,5,6};
+
+reverse(arr);
+}
+}
