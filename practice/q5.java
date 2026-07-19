5f82950ec52d1d80b0f7a4dccf5ccb9f6a93bce7
Paras <parasparte653@gmail.com>
2026-07-19 10:00:24 +0530

    q5

---
+++ practice/q5.java
@@ -0,0 +1,17 @@
+// Calculate the factorial of a number using a loop (not recursion)
+public class q5{
+
+    public static int  factorial(int n){
+        int a=1;
+        for(int i=2;i<=n;i++){
+            a*=i;
+        }
+        return a;
+        
+    }
+public static void main(String args[]){
+
+    int n=4;
+
+}
+}
