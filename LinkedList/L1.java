0dc85a47f89c18b2fc02495d3321284db6059043
Paras <parasparte653@gmail.com>
2026-07-28 19:07:57 +0530

    modified:   LinkedList/L1.class         modified:   LinkedList/L1.java

--- LinkedList/L1.java
+++ LinkedList/L1.java
@@ -75,17 +75,35 @@ public class L1{
             System.out.println();
         }
 
+        public void reverse(){
+            Node prev=null;
+            Node curr=tail=head;
+            Node next;
+            while(curr!=null){  // recite this 
+                next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+
+            }
+            head=prev;
+
+
+        }
+
 
     public static void main(String args[]){
         L1 ll=new L1();
-        ll.print();
+       
         ll.addFirst(1);
-        ll.print();
+        
         ll.addFirst(2);
-        ll.print();
+       
         ll.addLast(3);
-        ll.print();
+        
         ll.addLast(4);
+        ll.print();
+        ll.reverse();
         ll.print();
 
     }
