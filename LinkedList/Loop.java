d336b1fc397b7f027a8ca1917d31af71da654507
Paras <parasparte653@gmail.com>
2026-08-01 20:57:25 +0530

    CycleLoop

--- LinkedList/Loop.java
+++ LinkedList/Loop.java
@@ -42,7 +42,7 @@ public static void main(String args[]){
 head=new Node(1);
 head.next=new Node(2);
 head.next.next=new Node(3);
-head.next.next.next=head;
+//head.next.next.next=head;
 
 System.out.println(isCycle());
 
