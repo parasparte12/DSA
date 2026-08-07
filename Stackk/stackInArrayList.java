2a2d9810194d1675799b1d597e082641e6ae4276
Paras <parasparte653@gmail.com>
2026-08-07 18:14:44 +0530

    Stackk/

---
+++ Stackk/stackInArrayList.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class stackInArrayList {
+
+    static class Stack{
+
+        static ArrayList<Integer> list=new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size==0;
+        }
+
+    }
+
+    public static void main(String args[]){
+
+    }
+    
+}
