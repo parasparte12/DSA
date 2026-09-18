8670486e7880eb8f5ef4cd7da99046b9528f0e8c
parasparte12 <parasparte653@gmail.com>
2026-09-18 23:35:19 +0530

    Update is-subsequence.java

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-       int a=0;
-       int b=0;
+       int a=0; // for s string
+       int b=0;//  for t string
        while(a<s.length() && b<t.length()){
         if(s.charAt(a)==t.charAt(b)){
             a++;
